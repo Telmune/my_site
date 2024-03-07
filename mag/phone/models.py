6cc402206e82139611a742e59eea3a8f3ec8da11
Batko <goga_kozyrev@bk.ru>
2024-03-07 17:13:32 +0300

    detail styled template

--- mag/phone/models.py
+++ mag/phone/models.py
@@ -5,6 +5,7 @@ class Product(models.Model):
     title = models.CharField(max_length=100)
     price = models.IntegerField()
     descriptions = models.CharField(max_length=250)
+    image = models.ImageField(blank=True, upload_to='images')
 
     def __str__(self):
         return self.title
